87c81191bad3d0d0d2b5d98dfd1fd8c092dd6a4c
CTpeJlok90D <S90Dima@yandex.com>
2023-05-10 16:46:03 +0300

    particles

--- Assets/Gates/Gates.cs
+++ Assets/Gates/Gates.cs
@@ -4,6 +4,7 @@ public class Gates : MonoBehaviour
 {
 	[SerializeField] private TriggerEvents _trigger;
 	[SerializeField] private TeamScore _score;
+	[SerializeField] private ParticleSystem _particleSystem;
 
 	private void OnEnable()
 	{
@@ -15,6 +16,8 @@ public class Gates : MonoBehaviour
 		if (other.TryGetComponent(out Ball ball) && ball.isActiveAndEnabled)
 		{
 			_score.Current++;
+			_particleSystem.transform.position = ball.transform.position;
+			_particleSystem.Play();
 		}
 	}
 }
